38fdf261b085a693f68230c928cbd446e6d02cfc
telamonian <telamonian@users.noreply.github.com>
2021-01-31 02:50:15 -0500

    add some general RegularTable utils

--- packages/tree-finder/src/util.ts
+++ packages/tree-finder/src/util.ts
@@ -4,6 +4,8 @@
 | This file is part of the tree-finder library, distributed under the terms of
 | the BSD 3 Clause license. The full license can be found in the LICENSE file.
 |----------------------------------------------------------------------------*/
+import { MetaData, RegularTableElement } from "regular-table";
+
 export namespace Random {
   export function bool() {
     return Math.random() < 0.5;
@@ -20,5 +22,56 @@ export namespace Random {
     }
 
     return arr;
+  }
+}
+
+export namespace RegularTable {
+  /*
+   * click-related functions
+   */
+
+  export function cellClicked(metadata: MetaData): boolean {
+    return typeof metadata?.y !== "undefined" && !metadata?.column_header_y;
+  }
+
+  export function columnHeaderClicked(metadata: MetaData): boolean {
+    return typeof (metadata as any)?.row_header_y !== "undefined" && !!metadata?.column_header;
+  }
+
+  export function rowClicked(metadata: MetaData): boolean {
+    return cellClicked(metadata) || rowHeaderClicked(metadata);
+  }
+
+  export function rowHeaderClicked(metadata: MetaData): boolean {
+    return typeof (metadata as any)?.row_header_x !== "undefined" && !!metadata?.row_header;
+  }
+
+  /*
+   * debug utils
+   */
+
+  export function clickLoggingListener(event: MouseEvent, rt: RegularTableElement) {
+    const metadata = metadataFromEvent(event, rt);
+
+    if (cellClicked(metadata)) {
+      console.log(`cell clicked`);
+    }
+    if (columnHeaderClicked(metadata)) {
+      console.log(`column header clicked`);
+    }
+    if (rowClicked(metadata)) {
+      console.log(`row clicked`);
+    }
+    if (rowHeaderClicked(metadata)) {
+      console.log(`row header clicked`);
+    }
+  }
+
+  /*
+   * general metadata-related functions
+   */
+
+  export function metadataFromEvent(event: Event, rt: RegularTableElement) {
+    return rt.getMeta(event.target as HTMLTableCellElement);
   }
 }
