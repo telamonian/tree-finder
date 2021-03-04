d49dddfcf37c435535a8a37be6c062e921abb169
telamonian <telamonian@users.noreply.github.com>
2021-03-03 23:17:28 -0500

    add webpack build to new tree-finder-mockcontents package

---
+++ packages/tree-finder-mockcontents/webpack.config.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020, Max Klein
+ *
+ * This file is part of the tree-finder library, distributed under the terms of
+ * the BSD 3 Clause license. The full license can be found in the LICENSE file.
+ */
+const path = require("path");
+
+const { dependencySrcMapRules, getOptimization, getResolve } = require("../../webpack.rules");
+
+const isProd = process.env.NODE_ENV === "production";
+
+const treeFinderMockcontentsConfig = {
+  entry: {
+    "tree-finder-mockcontents": "src/index.ts",
+  },
+  devtool: "source-map",
+  context: path.resolve(__dirname, "../.."),
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      ...dependencySrcMapRules,
+    ],
+  },
+
+  resolve: {
+    ...getResolve(__dirname),
+  },
+
+  mode: isProd ? "production": "development",
+
+  optimization: {
+    minimize: isProd,
+    ...isProd && getOptimization(),
+  },
+};
+
+module.exports = [
+  treeFinderMockcontentsConfig,
+]
